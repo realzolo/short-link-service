d7cdb7a7e2543991ba71fe030ada203171134837
Zolo <zolo@onezol.com>
2022-07-09 21:07:14 +0800

    changed the way it was built

---
+++ project-builder.ts
@@ -0,0 +1,13 @@
+import fse from 'fs-extra';
+
+const files = [
+    {src: "./.env", dest: "./build/.env"},
+    {src: "./package.json", dest: "./build/package.json"},
+    {src: "./public", dest: "./build/public"},
+]
+
+files.forEach(file => {
+    fse.copySync(file.src, file.dest);
+});
+
+fse.removeSync("./build/project-builder.js");
